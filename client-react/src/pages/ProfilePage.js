311d646e7ac14e7c311b10d61978da080ed1d3fd
Gaspar <gasparcastello@gmail.com>
2022-03-29 13:58:44 -0300

    profile talent draft

--- client-react/src/pages/ProfilePage.js
+++ client-react/src/pages/ProfilePage.js
@@ -1,13 +1,16 @@
 import { Fragment } from "react";
 import { Col, Container, Row } from "reactstrap";
+import Profile from "../components/Register/Profile";
 import RegisterRecruiter from "../components/Register/RegisterRecruiter";
 import RegisterTalent from "../components/Register/RegisterTalent";
 
 export default function ProfilePage() {
   return (
     <Fragment>
-      {!false && <RegisterTalent></RegisterTalent>}
-      {true && <RegisterRecruiter></RegisterRecruiter>}
+      <Profile></Profile>
+      {/* editing and first login */}
+      {!true && <RegisterTalent></RegisterTalent>}
+      {!true && <RegisterRecruiter></RegisterRecruiter>}
     </Fragment>
   );
 }
