1be8b6d9abbfdc9d77fe1f26294dcb835dee6841
matiassemelman <matias.semelman@protonmail.com>
2022-03-29 16:02:34 -0300

    Added avatar example

--- client-react/src/pages/HomePage/FeaturedCard.js
+++ client-react/src/pages/HomePage/FeaturedCard.js
@@ -1,35 +1,30 @@
-import {
-  Col,
-  Container,
-  Row,
-  Fragment,
-  CardTitle,
-  CardText,
-  Button,
-  Card,
-} from "reactstrap";
+import { Col, Row, CardTitle, CardText, Card } from "reactstrap";
 
-export const FeaturedCard = () => {
+export const FeaturedCard = ({ color }) => {
   return (
     <Col>
-    <Row>
-      <a href="#">
-        <Col sm="12">
-          <Card body>
-            <Row>
-              <Col>Soy un avatar</Col>
-              <Col>
-                <CardTitle tag="h5">Special Title Treatment</CardTitle>
-                <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </CardText>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </a>
-    </Row>
+      <Row>
+        <a href="#">
+          <Col sm="12">
+            <Card body color={color}>
+              <Row>
+                <Col sm="3">
+                  <img
+                    src="https://cdn1.iconfinder.com/data/icons/social-black-buttons/512/anonymous-512.png"
+                    width="96px"
+                    height="96px"
+                    style={{borderRadius : "20px"}}
+                  />
+                </Col>
+                <Col>
+                  <CardTitle tag="h5">Job Title</CardTitle>
+                  <CardText>Remote/Location - Fulltime - 1500USD</CardText>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </a>
+      </Row>
     </Col>
   );
 };
