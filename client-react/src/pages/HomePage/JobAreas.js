a415635f96c24e07dabdc09bd38557991ccaab95
matiassemelman <matias.semelman@protonmail.com>
2022-03-29 14:53:04 -0300

    Componetization  for better readability

---
+++ client-react/src/pages/HomePage/JobAreas.js
@@ -0,0 +1,13 @@
+const JobAreas = () => {
+  return (
+    <>
+      <a href="#">Design / UX</a> <a href="#">Programming</a>{" "}
+      <a href="#">Data Science / Analytics</a>{" "}
+      <a href="#">Mobile Development</a> <a href="#">Digital Marketing</a>{" "}
+      <a href="#">SysAdmin / DevOps / QA</a> <a href="#">Sales</a>{" "}
+      <a href="#">Product, Innovation & Agile</a>
+    </>
+  );
+};
+
+export default JobAreas;
