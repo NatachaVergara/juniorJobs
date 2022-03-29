ce510424db8fe9e8347a2024e402e5488e340e4c
Gaspar <gasparcastello@gmail.com>
2022-03-29 12:01:15 -0300

    forms finished

--- client-react/src/components/Register/RegisterRecruiter.js
+++ client-react/src/components/Register/RegisterRecruiter.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Formik, Form, useField } from "formik";
 import * as Yup from "yup";
-import classes from "./RegisterForm.module.css";
-import { Input, Label } from "reactstrap";
+// import classes from "./RegisterTalent.module.css";
+import { Button, Col, Container, Input, Label, Row } from "reactstrap";
 
 const MyTextInput = ({ label, ...props }) => {
   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
@@ -11,20 +11,30 @@ const MyTextInput = ({ label, ...props }) => {
   return (
     <>
       <Label htmlFor={props.id || props.name}>{label}</Label>
-      <Input {...field} {...props} />
+      <Input
+        {...field}
+        {...props}
+        invalid={meta.error && meta.touched}
+        valid={!meta.error && meta.touched}
+      />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
     </>
   );
 };
-
 const MySelect = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <>
       <Label htmlFor={props.id || props.name}>{label}</Label>
-      <Input type="select" {...field} {...props} />
+      <Input
+        type="select"
+        {...field}
+        {...props}
+        invalid={meta.error && meta.touched}
+        valid={!meta.error && meta.touched}
+      />
       {meta.touched && meta.error ? <p>{meta.error}</p> : null}
     </>
   );
@@ -34,7 +44,13 @@ const MyCheckbox = ({ children, ...props }) => {
   return (
     <>
       <Label>
-        <Input {...field} {...props} type="checkbox" />
+        <Input
+          {...field}
+          {...props}
+          type="checkbox"
+          invalid={meta.error && meta.touched}
+          valid={!meta.error && meta.touched}
+        />
         {children}
       </Label>
       {meta.touched && meta.error ? (
@@ -43,27 +59,35 @@ const MyCheckbox = ({ children, ...props }) => {
     </>
   );
 };
-const MyRange = ({ label, ...props }) => {
-  const [field, meta] = useField({ ...props, type: "range" });
-  return (
-    <>
-      <Label htmlFor={props.id || props.name}>
-        {label}, is <b>{field.value}</b>
-      </Label>
-      <Input type="range" {...field} {...props} />
+// const MyRange = ({ label, ...props }) => {
+//   const [field, meta] = useField({ ...props, type: "range" });
+//   return (
+//     <>
+//       <Label htmlFor={props.id || props.name}>
+//         {label}, is <b>{field.value}</b>
+//       </Label>
+//       <Input type="range" {...field} {...props} />
 
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
+//       {meta.touched && meta.error ? (
+//         <div className="error">{meta.error}</div>
+//       ) : null}
+//     </>
+//   );
+// };
 const MyRadio = ({ label, ...props }) => {
   const [field, meta] = useField({ ...props, type: "radio" });
   return (
     <div>
       <Label>
-        <Input {...field} {...props} type="radio" />
+        <Container>
+          <Input
+            {...field}
+            {...props}
+            type="radio"
+            invalid={meta.error && meta.touched}
+            valid={!meta.error && meta.touched}
+          />
+        </Container>
         {label}
       </Label>
 
@@ -73,133 +97,115 @@ const MyRadio = ({ label, ...props }) => {
     </div>
   );
 };
+const urlRegex =
+  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
+const phoneRegex =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-export default function RegisterForm(props) {
+export default function RegisterRecruiter(props) {
   return (
     <>
-      <h1 className="h1">Complete your profile!</h1>
+      <h1 className="h1">Complete your recruiter profile!</h1>
       <Formik
         initialValues={{
           userName: "",
-          userLastName: "", // added for our select
+          userLastName: "",
           email: "",
-          cellphone: "",
-          linkedInUrl: 120,
-          repositoryUrl: 120,
-          userAge: true,
-          seniority: "",
-          experience: "",
-          education: "",
-          especiality: "",
-          acceptedTerms: false, // added for our checkbox
+          phone: "",
+          userAge: 18,
+          company: "",
+          companyDescription: "",
+          acceptedTerms: false,
         }}
         validationSchema={Yup.object({
-          userName: Yup.string().min(2, "Must be 2 characters or more"),
-          // .required("Required"),
-          cellphone: Yup.string()
-            .min(2, "Must be 2 characters or more")
-            .matches(
-              /^[abcdefghijklmnopqrstuvwxyz,]+$/,
-              "Please enter online valid characters, comma separated"
-            ),
-          // .required("Required"),
-          email: Yup.string(),
-          //   .required("Required"),
-          acceptedTerms: Yup.boolean()
-            // .required("Required")
-            .oneOf([true], "You must accept the terms and conditions."),
+          userName: Yup.string()
+            .min(4, "Must be 4 characters or more")
+            .required("Required"),
           userLastName: Yup.string()
-            // specify the set of valid values for job type
-            // @see http://bit.ly/yup-mixed-oneOf
-            .oneOf(
-              [
-                "african",
-                "american",
-                "greek",
-                "italian",
-                "Latin American",
-                "indian",
-                "french",
-              ],
-              "Invalid Job Type"
-            ),
-          // .required("Required"),
-          //   carbs: Yup.number().required("Required"),
+            .min(4, "Must be 4 characters or more")
+            .required("Required"),
+          email: Yup.string()
+            .email("Field should contain a valid e-mail")
+            .max(255)
+            .required("E-mail is required"),
+          phone: Yup.string().matches(phoneRegex, "Phone number is not valid"),
+          userAge: Yup.number().moreThan(17, "Enter a number greater than 18"),
+          company: Yup.string()
+            .max(350, "Must be 350 characters or less")
+            .required("Required"),
+          acceptedTerms: Yup.boolean()
+            .required("Required")
+            .oneOf([true], "You must accept the terms and conditions."),
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          setSubmitting(true);
-          props.onSubmit(values);
-          setSubmitting(false);
-        }}
         // onSubmit={(values, { setSubmitting }) => {
-        //   console.log(values);
-        //   setTimeout(() => {
-        //     alert(JSON.stringify(values, null, 2));
-        //     setSubmitting(false);
-        //   }, 400);
+        //   setSubmitting(true);
+        //   props.onSubmit(values);
+        //   setSubmitting(false);
         // }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values);
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
       >
         {({ isSubmitting, isValid }) => (
           <Form>
-            <MyTextInput
-              label="Title"
-              name="userName"
-              type="text"
-              placeholder="part of the title"
-            />
-            <MyTextInput
-              label="Ingredients"
-              name="cellphone"
-              type="text"
-              placeholder="ingredients"
-            />
-            <MySelect label="userLastName" name="userLastName">
-              <option value="">Select a userLastName type</option>
-              <option value="african">African</option>
-              <option value="american">American</option>
-              <option value="italian">Italian</option>
-              <option value="greek">Greek</option>
-              <option value="Latin American">Latin american</option>
-              <option value="indian">Indian</option>
-              <option value="french">French</option>
-            </MySelect>
-
-            <MyRange
-              label="Max carbs in grams "
-              name="linkedInUrl"
-              type="range"
-              max="120"
-              min="5"
-            />
-
-            <div className="radios">
-              <MyRadio
-                label="Gluten free"
-                name="email"
-                type="radio"
-                value="gluten-free"
-              />
-
-              <MyRadio label="Paleo" name="email" type="radio" value="paleo" />
-              <MyRadio label="Vegan" name="email" type="radio" value="vegan" />
-              <MyRadio
-                label="Vegetarian"
-                name="email"
-                type="radio"
-                value="vegetarian"
-              />
-              <MyCheckbox name="acceptedTerms">
-                I accept the terms and conditions
-              </MyCheckbox>
+            <Row>
+              <Col>
+                <MyTextInput label="Name" name="userName" type="text" />
+              </Col>
+              <Col>
+                <MyTextInput
+                  label="Last name"
+                  name="userLastName"
+                  type="text"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <MyTextInput
+                  label="E-mail"
+                  name="email"
+                  type="text"
+                  placeholder="example@juniorjobs.com"
+                />
+              </Col>
+              <Col>
+                <MyTextInput
+                  label="Phone number"
+                  name="phone"
+                  type="text"
+                  placeholder="+54 342 6 156 014"
+                />
+              </Col>
+              <Col>
+                <MyTextInput label="Age" name="userAge" type="number" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <MyTextInput
+                  label="Profile"
+                  name="company"
+                  type="textarea"
+                  placeholder="Describe the company"
+                />
+              </Col>
+            </Row>
+            <MyCheckbox name="acceptedTerms">
+              {" I accept the terms and conditions"}
+            </MyCheckbox>
+            <div>
+              <Button color="primary" disabled={isSubmitting || !isValid}>
+                Submit
+              </Button>
+              <Button color="danger" disabled={isSubmitting || !isValid}>
+                Remove this profile
+              </Button>
             </div>
-
-            <button
-              className={`btn btn-primary ${classes.button}`}
-              type="submit"
-              disabled={isSubmitting || !isValid}
-            >
-              SUBMIT
-            </button>
           </Form>
         )}
       </Formik>
--- client-react/src/pages/ProfilePage.js
+++ client-react/src/pages/ProfilePage.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
 import { Col, Container, Row } from "reactstrap";
+import RegisterRecruiter from "../components/Register/RegisterRecruiter";
 import RegisterTalent from "../components/Register/RegisterTalent";
 
 export default function ProfilePage() {
-  return <RegisterTalent></RegisterTalent>;
+  return (
+    <Fragment>
+      {!false && <RegisterTalent></RegisterTalent>}
+      {true && <RegisterRecruiter></RegisterRecruiter>}
+    </Fragment>
+  );
 }
