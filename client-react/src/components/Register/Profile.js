60b34c18d045e1ec289425bcf43f9d6a3873c6e7
Gaspar <gasparcastello@gmail.com>
2022-03-30 08:16:09 -0300

    profile talent finished

--- client-react/src/components/Register/Profile.js
+++ client-react/src/components/Register/Profile.js
@@ -12,86 +12,21 @@ import {
   CardImg,
 } from "reactstrap";
 import classes from "./Profile.module.scss";
+import ProfileCard from "./ProfileCard";
+import ProjectsCard from "./ProjectsCard";
 
 export default function Profile() {
   return (
     <Fragment>
       <Container>
         <Row>
-          <Col lg="8">
-            <Card body color="dark" outline>
-              <CardBody>
-                <CardTitle tag="h5">Projects</CardTitle>
-
-                <CardText>Mostrar proyectos realizados</CardText>
-                <div>
-                  <Card body color="dark" outline>
-                    <CardBody>
-                      <CardTitle tag="h5">Card title</CardTitle>
-                      <CardSubtitle className="mb-2 text-muted" tag="h6">
-                        Card subtitle
-                      </CardSubtitle>
-                      <CardText>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                      </CardText>
-                      <Button>Button</Button>
-                    </CardBody>
-                  </Card>
-                </div>
-                <div>
-                  <Card body color="dark" outline>
-                    <CardBody>
-                      <CardTitle tag="h5">Card title</CardTitle>
-                      <CardSubtitle className="mb-2 text-muted" tag="h6">
-                        Card subtitle
-                      </CardSubtitle>
-                      <CardText>
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
-                      </CardText>
-                      <Button>Button</Button>
-                    </CardBody>
-                  </Card>
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
           <Col>
-            <Card body color="dark" outline className={classes.card}>
-              <CardImg
-                src="https://media.istockphoto.com/photos/young-character-actor-headshot-picture-id1211061995?k=20&m=1211061995&s=170667a&w=0&h=KtUBCp33OpGS9VfQ35IIJB8jF7aC1r7tMhI_Rqlxh5I="
-                width="100%"
-                top
-              ></CardImg>
-              <CardTitle tag="h5">Pepe el programador</CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                About me
-              </CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Contactos
-              </CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Tech stack
-              </CardSubtitle>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-            </Card>
+            <ProfileCard />
+          </Col>
+          <Col lg="8">
+            <ProjectsCard />
           </Col>
         </Row>
-
-        <Button color="dark">EDIT</Button>
-        <Button color="dark">DELETE</Button>
       </Container>
     </Fragment>
   );
