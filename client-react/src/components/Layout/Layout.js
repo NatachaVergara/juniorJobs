12c2367b36050007dc270406f16d3f72c6d40a9c
Natacha Vergara <ntchvergara@gmail.com>
2022-03-31 19:04:12 -0300

    validating if the user is  logged in or not

--- client-react/src/components/Layout/Layout.js
+++ client-react/src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
 import { Fragment } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
-
 import MainNavigation from "./MainNavigation";
 import Sidebar from "./Sidebar";
 
 const Layout = (props) => {
+  
   return (
     <Fragment>
       <MainNavigation />
       <Container fluid>
         <Row>
-          <Sidebar />
+         <Sidebar />          
           <Col
             tag="main"
             md={{
--- client-react/src/components/Layout/Sidebar.js
+++ client-react/src/components/Layout/Sidebar.js
@@ -1,64 +1,79 @@
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Nav, NavItem, NavLink, Row } from "reactstrap";
 import classes from "./Sidebar.module.scss";
 import { MdOutlineCases,MdList,MdPersonOutline,MdBuild} from "react-icons/md";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (props) {
+ let [isLG, setIsLG] = useState(false)
+  console.log(isLG)
+
+  useEffect(() => {
+    setIsLG(JSON.parse(localStorage.getItem('isLogin')))
+   }, [isLG])
+
+
   return (
-    <Col tag="nav" xs="2" className="bg-secondary">
-      <Row>
-        <Nav vertical>
-          <NavItem>
-            <NavLink className={classes.anchor} href="/home">
-              <Container fluid>
-                <Row>
-                  <Col sm="2">
-                    <MdOutlineCases />
-                  </Col>
-                  <Col>Jobs for you</Col>
-                </Row>
-              </Container>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={classes.anchor} href="/home">
-              <Container fluid>
-                <Row>
-                  <Col sm="2">
-                    <MdList />
-                  </Col>
-                  <Col>Jobs applied</Col>
-                </Row>
-              </Container>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={classes.anchor} href="/home">
-              <Container fluid>
-                <Row>
-                  <Col sm="2">
-                    <MdPersonOutline />
-                  </Col>
-                  <Col>Profile</Col>
-                </Row>
-              </Container>
-            </NavLink>
-          </NavItem>
-          
-          <NavItem>
-            <NavLink className={classes.anchor} href="/home">
-              <Container fluid>
-                <Row>
-                  <Col sm="2">
-                    <MdBuild />
-                  </Col>
-                  <Col>Resources</Col>
-                </Row>
-              </Container>
-            </NavLink>
-          </NavItem>
-          
-        </Nav>
-      </Row>
-    </Col>
-  );
+   <>   
+      {isLG && <Col tag="nav" xs="2" className="bg-secondary">
+        <Row>
+          <Nav vertical>
+            <NavItem>
+              <NavLink className={classes.anchor} href="/home">
+                <Container fluid>
+                  <Row>
+                    <Col sm="2">
+                      <MdOutlineCases />
+                    </Col>
+                    <Col>Jobs for you</Col>
+                  </Row>
+                </Container>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className={classes.anchor} href="/home">
+                <Container fluid>
+                  <Row>
+                    <Col sm="2">
+                      <MdList />
+                    </Col>
+                    <Col>Jobs applied</Col>
+                  </Row>
+                </Container>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className={classes.anchor} href="/home">
+                <Container fluid>
+                  <Row>
+                    <Col sm="2">
+                      <MdPersonOutline />
+                    </Col>
+                    <Col>Profile</Col>
+                  </Row>
+                </Container>
+              </NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink className={classes.anchor} href="/home">
+                <Container fluid>
+                  <Row>
+                    <Col sm="2">
+                      <MdBuild />
+                    </Col>
+                    <Col>Resources</Col>
+                  </Row>
+                </Container>
+              </NavLink>
+            </NavItem>
+
+          </Nav>
+        </Row>
+      </Col>
+ }
+   
+  
+    </>
+  )
 }
