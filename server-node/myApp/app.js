ab81f7d20531d0bf345efda5403817c29e827017
HernanGarcia1985 <hernang1985@gmail.com>
2022-04-01 16:00:06 -0300

    Se corrigió la ruta de views en app.js porque daba error 404

--- server-node/myApp/app.js
+++ server-node/myApp/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3002
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
