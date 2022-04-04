cb8669f5fdb628ea7ad3932b49617fb540308af2
matiassemelman <matias.semelman@protonmail.com>
2022-04-04 15:10:18 -0300

    Move hard-data to this external file

---
+++ client-react/src/components/Resources/dbResources.js
@@ -0,0 +1,38 @@
+export let frontendResources = [  {
+    title: "Introduction to CSS3",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/fjm9ztcwpy8n5x71/w460_fjm9ztcwpy8n5x71.jpeg",
+    description: "The web today is almost unrecognizable from the early days of white pages with lists of blue links.The emphasis will be on learning how to write CSS rules, how to test code, and how to establish good programming habits.",
+    link: "https://www.coursera.org/learn/introcss",
+  },
+  {
+    title: "The Basics of Web-Design Layout",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/63mnz5zkay1saadb/w460_63mnz5zkay1saadb.jpg",
+    description: "Learn How To Describe Your Design With HTML & CSS. This class will show you how to describe layouts to the browser, what the defaults are, how to override them, and when to override them.",
+    link: "https://www.skillshare.com/classes/The-Basics-of-Web-Design-Layout-Learn-How-To-Describe-Your-Design-With-HTML-CSS/1282829766?via=search-layout-grid",
+  },
+  {
+    title: "Intro to HTML Email Development",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/pl0aivg4ajriqnvc/w460_pl0aivg4ajriqnvc.jpeg",
+    description: "This class is geared toward anyone that wants to learn more about how emails are developed with HTML. Having previous knowledge of HTML is preferred, but anyone without prior knowledge is welcome to take the course and follow along.",
+    link: "https://www.skillshare.com/classes/Intro-to-HTML-Email-Development/1851764446?via=search-layout-grid",
+  },
+  {
+    title: "Web Development Fundamentals",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/6a09bpd04sbau16p/w460_6a09bpd04sbau16p.jpeg",
+    description: "This class is the next step in our Web Development Fundamentals series. In this class, we're gonna learn how to add more interactive features to our websites and web applications with Javascript.",
+    link: "https://www.skillshare.com/classes/Web-Development-Fundamentals-Javascript/342157332?via=search-layout-grid",
+  },
+  {
+    title: "Python for Everybody",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/581rii9482avqjc8/w460_581rii9482avqjc8.jpeg",
+    description: "Learn Python for Everybody from University of Michigan. This Specialization builds on the success of the Python for Everybody course and will introduce fundamental programming concepts including data structures, networked application program ...",
+    link: "https://www.coursera.org/specializations/python",
+  },
+  {
+    title: "Khan Academy SQL",
+    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/xf8w5zgbgk099lm0/w460_xf8w5zgbgk099lm0.jpeg",
+    description: "Learn how to use SQL to store, query, and manipulate data. SQL is a special-purpose programming language designed for managing data in a relational database, and is used by a huge number of apps and organizations.",
+    link: "https://www.khanacademy.org/computing/computer-programming/sql",
+  }
+]
+  
