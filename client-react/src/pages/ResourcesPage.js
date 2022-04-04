fd4ab4056195cbed650401d4c252eb07bf1ddf1e
matiassemelman <matias.semelman@protonmail.com>
2022-04-04 15:11:11 -0300

    Moved resources hard-data

--- client-react/src/pages/ResourcesPage.js
+++ client-react/src/pages/ResourcesPage.js
@@ -1,41 +1,16 @@
 import { Fragment } from "react";
 import { Col, Row } from "reactstrap";
 import IndividualResource from "../components/Resources/IndividualResource";
+import { frontendResources } from "../components/Resources/dbResources";
 
 
-let resources = [
-  {
-    title: "Introduction to CSS3",
-    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/fjm9ztcwpy8n5x71/w460_fjm9ztcwpy8n5x71.jpeg",
-    description: "The web today is almost unrecognizable from the early days of white pages with lists of blue links.The emphasis will be on learning how to write CSS rules, how to test code, and how to establish good programming habits.",
-    link: "https://www.coursera.org/learn/introcss",
-  },
-  {
-    title: "Introduction to CSS3",
-    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/fjm9ztcwpy8n5x71/w460_fjm9ztcwpy8n5x71.jpeg",
-    description: "The web today is almost unrecognizable from the early days of white pages with lists of blue links.The emphasis will be on learning how to write CSS rules, how to test code, and how to establish good programming habits.",
-    link: "https://www.coursera.org/learn/introcss",
-  },
-  {
-    title: "Introduction to CSS3",
-    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/fjm9ztcwpy8n5x71/w460_fjm9ztcwpy8n5x71.jpeg",
-    description: "The web today is almost unrecognizable from the early days of white pages with lists of blue links.The emphasis will be on learning how to write CSS rules, how to test code, and how to establish good programming habits.",
-    link: "https://www.coursera.org/learn/introcss",
-  },
-  {
-    title: "Introduction to CSS3",
-    imgLink: "https://diak46rl5chc7.cloudfront.net/orgs/75329/contents/fjm9ztcwpy8n5x71/w460_fjm9ztcwpy8n5x71.jpeg",
-    description: "The web today is almost unrecognizable from the early days of white pages with lists of blue links.The emphasis will be on learning how to write CSS rules, how to test code, and how to establish good programming habits.",
-    link: "https://www.coursera.org/learn/introcss",
-  }
-]
 
 const ResourcesPage = () => {
   return (
     <Fragment>
       <h2>Frontend Development</h2>
       <Row >
-{resources.map((r, i) => ( 
+{frontendResources.map((r, i) => ( 
 
     <Col>
 
