8a71472f2cf1fcbde2b94d11d586898e88496ead
Natacha Vergara <ntchvergara@gmail.com>
2022-04-05 10:57:39 -0300

    updating css in faq-page

--- client-react/src/components/FAQ/Faq.js
+++ client-react/src/components/FAQ/Faq.js
@@ -3,27 +3,24 @@ import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 import './Faq.scss'
 
 const Faq = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [id, setId] = useState('')
-
-    console.log(isOpen)
-    const onHandleOpen = (id) => {
-        setIsOpen(true)
-        setId(id)
-    }
+    const [clicked, setClicked] = useState(false)
 
 
-    const onHandleClose = (id) => {
-        setIsOpen(false)
-        setId(id)
-    }
+    const toggle = index => {
+        if (clicked === index) {
+            //if clicked question is already active, then close it
+            return setClicked(null);
+        }
+
+        setClicked(index);
+    };
 
 
     const faq = [
         {
             question: '1. Question 1',
             answer: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia iusto consequatur voluptatem eligendi doloremque fugit eaque quia eveniet! At quo iure commodi natus ullam quisquam architecto unde explicabo perferendis.',
-            id:'1'
+            id: '1'
         },
         {
             question: '2. Question 2',
@@ -40,7 +37,7 @@ const Faq = () => {
             answer: ' Accusantium? Placeat a illum, laborum delectus totam ex libero, perspiciatis molestias veniam asperiores dolor mollitia.Nam numquam accusamus voluptatibus.Nostrum inventore voluptatum in, odio quaerat architecto voluptas ad neque repellat sequi?',
             id: '4'
         }
-    
+
     ]
 
 
@@ -53,29 +50,16 @@ const Faq = () => {
                 <h2>Frequently Asked Questions</h2>
             </div>
             <div className='acordeon'>
-                {faq.map((qb, i) => (                    
+                {faq.map((qb, i) => (
                     <div className="questionBox" id={i} key={i}>
-                        <div className='questionHeader bg-success'>
-                            <h3>{ qb.question}</h3>
-                            {!isOpen ? <button className='btn' onClick={() => onHandleOpen(qb.id)}>
-                                <IoIosArrowDown />
-                            </button> : <button className='btn' onClick={() => onHandleClose('')} >
-                                <IoIosArrowUp />
-                            </button>}
+                        <div className='questionHeader bg-secondary' onClick={() => toggle(i)}>
+                            <h3 className='text-light'>{qb.question}</h3>
+                            <button className='btn text-light'>{clicked === i ? <IoIosArrowDown /> : <IoIosArrowUp />}   </button>
                         </div>
-                        {id === qb.id ? <p className='answer p-5'> {qb.answer} </p> : null}
+                        {clicked === i ? <p className='answer p-5'> {qb.answer} </p> : null}
                     </div>
                 ))}
-
-
-
-
-
-
-
             </div>
-
-
         </div>
     )
 }
