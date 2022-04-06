41d7be335d7b1ef10c8b432d77ff6ec5e3fa4178
Gaspar <gasparcastello@gmail.com>
2022-04-06 09:21:47 -0300

    footer finished

--- client-react/src/components/Layout/Footer.js
+++ client-react/src/components/Layout/Footer.js
@@ -11,37 +11,35 @@ import { Navbar, NavItem, NavLink } from "reactstrap";
 export default function Footer(props) {
   return (
     <Navbar tag="footer" className="mt-auto bg-secondary text-light ">
-      <NavItem>Development of JJ team</NavItem>
+      <NavItem>Developed by JJ team</NavItem>
       <NavItem>
         <NavLink to="/">
-          <span>
-            <TiSocialGithub />
-          </span>
+          <TiSocialGithub size="2em" color="white" />
         </NavLink>
       </NavItem>
       <NavItem>
         <NavLink to="#">
-          <TiSocialInstagram />
+          <TiSocialInstagram size="1.5em" color="white" />
         </NavLink>
       </NavItem>
       <NavItem>
         <NavLink to="#">
-          <TiSocialLinkedin />
+          <TiSocialLinkedin size="1.5em" color="white" />
         </NavLink>
       </NavItem>{" "}
       <NavItem>
         <NavLink to="#">
-          <TiSocialSkypeOutline />
+          <TiSocialSkypeOutline size="1.5em" color="white" />
         </NavLink>
       </NavItem>
       <NavItem>
         <NavLink to="#">
-          <TiSocialTwitter />
+          <TiSocialTwitter size="1.5em" color="white" />
         </NavLink>
       </NavItem>
       <NavItem>
         <NavLink to="#">
-          <TiSocialYoutube />
+          <TiSocialYoutube size="1.5em" color="white" />
         </NavLink>
       </NavItem>
     </Navbar>
