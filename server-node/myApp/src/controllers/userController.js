0d47cb0479ad938f6a8c814c65c15e7e95dd8468
matiasfeliu92 <matumazparrote@gmail.com>
2022-04-06 11:54:35 -0300

    modificacion de inicio de sesion en userController

--- server-node/myApp/src/controllers/userController.js
+++ server-node/myApp/src/controllers/userController.js
@@ -5,40 +5,75 @@ const User = require('../database/models/index')
 const userController = {
     login: (req, res) => {
 
-
+        console.log(req.body);
         let userType = req.body.userType; //Talent o Recruiter
 
-        db.userType.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        .then((user) =>{
-            if(user){
-                let validatePassword = bcryptjs.compareSync(req.body.password,user.password);
-                if (validatePassword == true){
-                    console.log("Bienvenido, " + req.body.email);
-                    return res.json("Bienvenido, " + req.body.email);
+        if(userType == 'Talent') {
+
+            db.Talent.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then((user) =>{
+                if(user){
+                    let validatePassword = bcryptjs.compareSync(req.body.password,user.password);
+                    if (validatePassword == true){
+                        console.log("Bienvenido, " + req.body.email);
+                        return res.json("Bienvenido, " + req.body.email);
+                    }
+                    else{
+                        console.log("Las credenciales son inválidas");
+                        return res.status(403).json(
+                            {message: "Las credenciales son inválidas",
+                            status: 403}
+                        );
+                    }
                 }
                 else{
-                    console.log("Las credenciales son inválidas");
-                    return res.status(403).json(
-                        {message: "Las credenciales son inválidas",
-                        status: 403}
+                    console.log("No se encuentra el email registrado en nuestra base de datos");
+                    return res.status(404).json(
+                        {message: "No se encuentra el email registrado en nuestra base de datos",
+                        status: 404}
                     );
                 }
-            }
-            else{
-                console.log("No se encuentra el email registrado en nuestra base de datos");
-                return res.status(404).json(
-                    {message: "No se encuentra el email registrado en nuestra base de datos",
-                    status: 404}
-                );
-            }
-        })
-        .catch(function(error){
-            console.log("Ocurrió un error por favor vuelva a intentarlo");
-        })    
+            })
+            .catch(function(error){
+                console.log("Ocurrió un error por favor vuelva a intentarlo");
+            })    
+        } else if(userType == 'Recruiter') {
+            db.Recruiter.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then((user) =>{
+                if(user){
+                    let validatePassword = bcryptjs.compareSync(req.body.password,user.password);
+                    if (validatePassword == true){
+                        console.log("Bienvenido, " + req.body.email);
+                        return res.json("Bienvenido, " + req.body.email);
+                    }
+                    else{
+                        console.log("Las credenciales son inválidas");
+                        return res.status(403).json(
+                            {message: "Las credenciales son inválidas",
+                            status: 403}
+                        );
+                    }
+                }
+                else{
+                    console.log("No se encuentra el email registrado en nuestra base de datos");
+                    return res.status(404).json(
+                        {message: "No se encuentra el email registrado en nuestra base de datos",
+                        status: 404}
+                    );
+                }
+            })
+            .catch(function(error){
+                console.log("Ocurrió un error por favor vuelva a intentarlo");
+            })   
+        }
     },
 
     // register: (req, res) => {
