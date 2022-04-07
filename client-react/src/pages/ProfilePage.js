654245263130d9f284e03df18fcc83f147ab8b88
Natacha Vergara <ntchvergara@gmail.com>
2022-04-07 12:22:02 -0300

    Conection with db success

--- client-react/src/pages/ProfilePage.js
+++ client-react/src/pages/ProfilePage.js
@@ -7,8 +7,8 @@ import { useAxios } from "../hooks/use-axios";
 export default function ProfilePage() {
   // pide context, porque necesito manejar autorizaciones y estados de signup/editing, talent/recruiter
   const { fetchData, response } = useAxios();
-  const [isSignup, setIsSignup] = useState(false);
-  const [isRecruiter, setIsRecruiter] = useState(true);
+  const [isSignup, setIsSignup] = useState(true);
+  const [isRecruiter, setIsRecruiter] = useState(false);
   function onEditHandler(props) {
     // setIsSignup(true);
   }
@@ -33,12 +33,26 @@ export default function ProfilePage() {
     } else {
       if (isSignup) {
         params.method = "post";
-        params.url = "/talent";
+        params.url = "/talents";
         params.headers = { accept: "*/*" };
-        params.data = { values };
+        params.data = {
+          name: "User3",
+          lastName: "Prueba",
+          email: "prueba3@gmail.com",
+          password: "pr123",
+          birthdate: "1986-04-26",
+          image: "https://www.tzuzulcode.com/_next/image?url=%2Flogo.png&w=64&q=75",
+          repository: "www.git.com",
+          url: "https://www.google.com.ar",
+          profile: "www.linkedin.com",
+          phone: "3115623245",
+          id_Seniority: 1,
+          id_Experience: 1,
+          id_Speciality: 1,
+          id_Education: 1 };
       } else {
         params.method = "put";
-        params.url = "/talent/:id";
+        params.url = "/talents/:id";
         params.headers = { accept: "*/*" };
         params.auth = {
           username: "",
@@ -47,20 +61,20 @@ export default function ProfilePage() {
         params.data = { values };
       }
     }
-    fetchData({ params });
+    fetchData({ ...params });
   }
 
   return (
     <Fragment>
       <Profile onEdit={onEditHandler} onDelete={onSubmitHandler}></Profile>
 
-      {isSignup && !isRecruiter && (
+      {/* {isSignup && !isRecruiter && ( */}
         <RegisterTalent onSubmit={onSubmitHandler}></RegisterTalent>
-      )}
+      {/* )} */}
 
-      {isSignup && isRecruiter && (
+      {/* {isSignup && isRecruiter && ( */}
         <RegisterRecruiter onSubmit={onSubmitHandler}></RegisterRecruiter>
-      )}
+      {/* )} */}
     </Fragment>
   );
 }
