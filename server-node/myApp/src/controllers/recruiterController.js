596a4850237d985fbf6e3eb3c97f732326177e50
HernanGarcia1985 <hernang1985@gmail.com>
2022-04-08 17:58:37 -0300

    Se creó metodo Show para Recruiter

--- server-node/myApp/src/controllers/recruiterController.js
+++ server-node/myApp/src/controllers/recruiterController.js
@@ -101,6 +101,21 @@ const recruiterController = {
 
     show: (req, res) => {
         //res.json("Metodo visualización de Recruiter x id");
+        db.Recruiter.findByPk(req.params.id)
+        .then((recruiter) => {
+            if(recruiter) {
+                delete recruiter.dataValues.password;
+                console.log(recruiter);
+                return res.status(200).json(recruiter.dataValues);
+            }
+            else {
+                console.log('No se encontró el reclutador en nuestra base de datos');
+                return res.status(404).json({message: 'No se encontró el reclutador en nuestra base de datos'});
+            }
+        })
+        .catch(function(error){
+            console.log(`Se ha producido el siguiente error: `, error);
+        })
     },
 
     index: (req, res) => {
