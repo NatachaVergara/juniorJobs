feabffc1d689ba188660e2918522f29b2e6a6d6e
HernanGarcia1985 <hernang1985@gmail.com>
2022-04-08 16:54:26 -0300

    Se creó metodo Update para Recruiter

--- server-node/myApp/src/controllers/recruiterController.js
+++ server-node/myApp/src/controllers/recruiterController.js
@@ -12,7 +12,7 @@ const recruiterController = {
         .then((recruiter) => {
             if(recruiter) {
                 console.log('Este usuario ya se encuentra registrado');
-                return res.json({message: 'Este usuario ya se encuentra registrado'});
+                return res.status(202).json({message: 'Este usuario ya se encuentra registrado'});
             }
             else {
                 const password = req.body.password;
@@ -27,11 +27,11 @@ const recruiterController = {
                     url: req.body.url
                 })
                 .then((recruiter) => {
-                    console.log('reclutador creado');
-                    res.json(recruiter);
+                    console.log('Reclutador creado');
+                    res.status(201).json({message: 'Reclutador creado'});
                 })
                 .catch(function(error){
-                    console.log("No se pudo crear el registro en la base de datos");
+                    console.log("No se pudo crear el registro en la base de datos", error);
                 })
             }
         })
@@ -41,7 +41,35 @@ const recruiterController = {
     },
 
     update: (req, res) => {
-        res.json("Metodo edición de Recruiter");
+        //res.json("Metodo edición de Recruiter");
+        console.log(req.body);
+        db.Recruiter.findByPk(req.params.id)
+        .then((recruiter) => {
+            if(recruiter) {
+                db.Recruiter.update({
+                    name: req.body.name,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    company: req.body.company,
+                    image: req.body.image,
+                    url: req.body.url
+                },{where: {id:req.params.id}})
+                .then(()=>{
+                    console.log('Se actualizó el reclutador');
+                    return res.status(201).json({message: 'Se actualizó el reclutador'});
+                })
+                .catch(function(error){
+                    console.log("No se pudo crear el registro en la base de datos", error);
+                })
+            }
+            else {
+                console.log('No se encontró el reclutador en nuestra base de datos');
+                return res.status(404).json({message: 'No se encontró el reclutador en nuestra base de datos'});
+            }
+        })
+        .catch(function(error){
+            console.log(`Se ha producido el siguiente error: `, error);
+        })
     },
 
     destroy: (req, res) => {
