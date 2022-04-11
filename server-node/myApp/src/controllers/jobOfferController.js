38213f3d2eb50aa3b79f99960c74a9f781129fb0
HernanGarcia1985 <hernang1985@gmail.com>
2022-04-11 12:53:21 -0300

    Se creó metodo Create para JobOffer

--- server-node/myApp/src/controllers/jobOfferController.js
+++ server-node/myApp/src/controllers/jobOfferController.js
@@ -2,7 +2,44 @@ const db = require('../database/models');
 
 const jobOfferController = {
     create: (req, res) => {
-        res.json("Metodo creación de una Oferta Laboral");
+        //res.json("Metodo creación de una Oferta Laboral");
+        console.log(req.body);
+
+        db.JobOffer.findOne({where: {
+            id_Speciality: req.body.id_Speciality,
+            id_Recruiter: req.body.id_Recruiter,
+            id_Talent: null
+        }})
+        .then((jobOffer) => {
+            if(jobOffer) {
+                console.log('Esta oferta de trabajo ya existe');
+                return res.status(202).json({message: 'Esta oferta de trabajo ya existe'});
+            }
+            else {
+                db.JobOffer.create({
+                    title: req.body.title,
+                    description: req.body.description,
+                    location: req.body.location,
+                    id_Recruiter: req.body.id_Recruiter,
+                    id_Schedule: req.body.id_Schedule,
+                    id_Remote: req.body.id_Remote,
+                    id_Talent: null,
+                    id_Seniority: req.body.id_Seniority,
+                    id_Experience: req.body.id_Experience,
+                    id_Speciality: req.body.id_Speciality
+                })
+                .then((jobOffer) => {
+                    console.log('Oferta de trabajo creada');
+                    res.status(201).json({message: 'Oferta de trabajo creada'});
+                })
+                .catch(function(error){
+                    console.log("No se pudo crear el registro en nuestra base de datos", error);
+                })
+            }
+        })
+        .catch(function(error){
+            console.log(`Se ha producido el siguiente error: `, error);
+        })
     },
 
     update: (req, res) => {
