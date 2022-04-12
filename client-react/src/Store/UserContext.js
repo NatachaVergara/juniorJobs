f5477cf25cff4327b7d27001e82e34c215cd4095
Gaspar <gasparcastello@gmail.com>
2022-04-12 11:13:37 -0300

    get on profile drafting

--- client-react/src/Store/UserContext.js
+++ client-react/src/Store/UserContext.js
@@ -1,52 +1,37 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-import React, { createContext, useContext, useEffect, useState } from 'react'
-
-
-const UserContext = createContext(null)
-//Esta es la funcion que tengo que llamar en los componentes 
+const UserContext = createContext(null);
+//Esta es la funcion que tengo que llamar en los componentes
 //const { } = useUserContext()
 export const useUserContext = () => {
-  return useContext(UserContext)
-}
+  return useContext(UserContext);
+};
 
 //Creo una funcion para utilizar el localStorage y que no se pierda el logueo del user cada vez que se refresca el sitio
 
 const getLocalUser = () => {
-  let user = localStorage.getItem('user')
+  let user = localStorage.getItem("user");
 
   if (user) {
-    return JSON.parse(localStorage.getItem('user'))
+    return JSON.parse(localStorage.getItem("user"));
   } else {
-    return false
+    return false;
   }
-}
-
+};
 
 const UserContextProvider = ({ children }) => {
-  const [isUser, setIsUser] = useState(getLocalUser())
-  const [userType, setUsertype]= useState('')
-  console.log(userType)
-
-
-  
+  const [isUser, setIsUser] = useState(getLocalUser());
+  const [userType, setUsertype] = useState("");
+  console.log(userType);
   //Creo un estado user dentro de mi localStorage
   useEffect(() => {
-    localStorage.setItem('user', JSON.stringify(isUser))
-  }, [isUser])
-
-
-
-
+    localStorage.setItem("user", JSON.stringify(isUser));
+  }, [isUser]);
   return (
-    <UserContext.Provider
-      value={{ isUser, setIsUser, setUsertype, userType }}
-    >
+    <UserContext.Provider value={{ isUser, setIsUser, setUsertype, userType }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
-
-
-
-export default UserContextProvider
+export default UserContextProvider;
