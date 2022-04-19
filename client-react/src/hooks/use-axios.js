1de60e53898352834572c9277e97d582104bde13
Gaspar <gasparcastello@gmail.com>
2022-04-19 09:20:51 -0300

    useAxios fixed

--- client-react/src/hooks/use-axios.js
+++ client-react/src/hooks/use-axios.js
@@ -1,27 +1,27 @@
 import axios from "axios";
-import { useState, useCallback,useEffect } from "react";
 import swal from "sweetalert";
+import { useState, useCallback } from "react";
 
-axios.defaults.baseURL = "https://api.spoonacular.com";
+axios.defaults.baseURL = "http://localhost:3002";
 
 export const useAxios = () => {
   const [response, setResponse] = useState(undefined);
-
   const fetchData = useCallback((params) => {
-    axios.request(params)
+    axios
+      .request(params)
       .then((res) => {
-          // console.log(res,'res on use-axios')
         setResponse(res.data);
       })
+
       .catch(function (error) {
-        console.log(error.response,error.message)
+        console.log(error.response, error.message);
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
           console.log(error.response.data);
           console.log(error.response.status);
           console.log(error.response.headers);
-          return swal(error.response.data.message, "from spoonacular", "warning");
+          return swal(error.response.data.message, "from database", "warning");
         } else if (error.request) {
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
@@ -31,7 +31,8 @@ export const useAxios = () => {
           // Something happened in setting up the request that triggered an Error
           return swal("Last Error", error.message);
         }
-      })
-  },[]);
+      });
+  }, []);
 
-  return {response,fetchData};
+  return { response, fetchData };
+};
