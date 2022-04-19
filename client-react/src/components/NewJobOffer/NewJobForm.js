b79849f0557c2339e523314b72804d98ddd6f07f
Gaspar <gasparcastello@gmail.com>
2022-04-19 10:02:05 -0300

    layout drafting

--- client-react/src/components/NewJobOffer/NewJobForm.js
+++ client-react/src/components/NewJobOffer/NewJobForm.js
@@ -20,131 +20,127 @@ const NewJobForm = () => {
     fetchData();
   }, []);
   return (
-    <Container>
-      <Formik
-        initialValues={{
-          title: "",
-          description: "",
-          location: "",
-          id_Recruiter: userID,
-          id_Talent: null,
-          id_Schedule: 1,
-          id_Remote: 1,
-          id_Seniority: 1,
-          id_Experience: 1,
-          id_Speciality: 1,
-          //   jobArea: 1,
-          //   checked: [],
-        }}
-        onSubmit={(values, { resetForm, isS }) => {
-          resetForm();
-          console.log("values on submit new job form", values);
-          const config = {
-            method: "POST",
-            url: "/jobOffers",
-            data: values,
-            header: { "Content-type": "application/json" },
-          };
-          fetchData(config);
-          console.log("response in new job form on submit", response);
-          setSendingForm(true);
-          setTimeout(() => setSendingForm(false), 3000);
-        }}
-      >
-        {() => (
-          <Form className="newjob-form">
-            <div>
-              <label htmlFor="title" className="formTitle">
-                Puesto / Título del aviso <span>*</span>
-              </label>
-              <Field type="textarea" id="title" name="title" />
-            </div>
-            <div>
-              <label htmlFor="description" className="formTitle">
-                Descripción del puesto <span>*</span>
-              </label>
-              <Field as="textarea" id="description" name="description" />
-            </div>
-            <div>
-              <label htmlFor="location" className="formTitle">
-                Descripción del puesto <span>*</span>
-              </label>
-              <Field as="textarea" id="location" name="location" />
-            </div>
+    <Formik
+      initialValues={{
+        title: "",
+        description: "",
+        location: "",
+        id_Recruiter: userID,
+        id_Talent: null,
+        id_Schedule: 1,
+        id_Remote: 1,
+        id_Seniority: 1,
+        id_Experience: 1,
+        id_Speciality: 1,
+        //   jobArea: 1,
+        //   checked: [],
+      }}
+      onSubmit={(values, { resetForm, isS }) => {
+        resetForm();
+        console.log("values on submit new job form", values);
+        const config = {
+          method: "POST",
+          url: "/jobOffers",
+          data: values,
+          header: { "Content-type": "application/json" },
+        };
+        fetchData(config);
+        console.log("response in new job form on submit", response);
+        setSendingForm(true);
+        setTimeout(() => setSendingForm(false), 3000);
+      }}
+    >
+      {() => (
+        <Form className="newjob-form">
+          <div>
+            <label htmlFor="title" className="formTitle">
+              Puesto / Título del aviso <span>*</span>
+            </label>
+            <Field type="textarea" id="title" name="title" />
+          </div>
+          <div>
+            <label htmlFor="description" className="formTitle">
+              Descripción del puesto <span>*</span>
+            </label>
+            <Field as="textarea" id="description" name="description" />
+          </div>
+          <div>
+            <label htmlFor="location" className="formTitle">
+              Descripción del puesto <span>*</span>
+            </label>
+            <Field as="textarea" id="location" name="location" />
+          </div>
 
-            <div>
-              <label className="formTitle" htmlFor="id_Schedule">
-                Schedule
-              </label>
-              <Field name="id_Schedule" as="select" id="id_Schedule">
-                <option value={0}>Presencial</option>
-                <option value={1}>Hybrid</option>
-                <option value={2}>Remote</option>
-              </Field>
-            </div>
-            <div>
-              <label className="formTitle" htmlFor="id_Remote">
-                Remote
-              </label>
-              <Field name="id_Remote" as="select" id="id_Remote">
-                <option value={0}>Si</option>
-                <option value={1}>No</option>
-              </Field>
-            </div>
+          <div>
+            <label className="formTitle" htmlFor="id_Schedule">
+              Schedule
+            </label>
+            <Field name="id_Schedule" as="select" id="id_Schedule">
+              <option value={0}>Presencial</option>
+              <option value={1}>Hybrid</option>
+              <option value={2}>Remote</option>
+            </Field>
+          </div>
+          <div>
+            <label className="formTitle" htmlFor="id_Remote">
+              Remote
+            </label>
+            <Field name="id_Remote" as="select" id="id_Remote">
+              <option value={0}>Si</option>
+              <option value={1}>No</option>
+            </Field>
+          </div>
 
-            <div>
-              <label className="formTitle" htmlFor="id_Seniority">
-                Seniority
-              </label>
-              <Field name="id_Seniority" as="select" id="id_Seniority">
-                {seniorities &&
-                  seniorities.length > 0 &&
-                  seniorities.map((seniority, index) => {
-                    return (
-                      <option value={index} key={index}>
-                        {seniority.name}
-                      </option>
-                    );
-                  })}
-              </Field>
-            </div>
-            <div>
-              <label className="formTitle" htmlFor="id_Experience">
-                Experience
-              </label>
-              <Field name="id_Experience" as="select" id="id_Experience">
-                <option value={0}>0-2 months</option>
-                <option value={1}>2-6 months</option>
-                <option value={2}>6-12 monts</option>
-                <option value={3}>1-2 years</option>
-                <option value={4}>2-4 years</option>
-              </Field>
-            </div>
-            <div>
-              <label className="formTitle" htmlFor="id_Speciality">
-                Speciality
-              </label>
-              <Field name="id_Speciality" as="select" id="id_Speciality">
-                <option value={0}>Administrador de Base de Datos/DBA</option>
-                <option value={1}>Administrador de Infraestructura</option>
-                <option value={2}>Administrador de Redes</option>
-              </Field>
-            </div>
+          <div>
+            <label className="formTitle" htmlFor="id_Seniority">
+              Seniority
+            </label>
+            <Field name="id_Seniority" as="select" id="id_Seniority">
+              {seniorities &&
+                seniorities.length > 0 &&
+                seniorities.map((seniority, index) => {
+                  return (
+                    <option value={index} key={index}>
+                      {seniority.name}
+                    </option>
+                  );
+                })}
+            </Field>
+          </div>
+          <div>
+            <label className="formTitle" htmlFor="id_Experience">
+              Experience
+            </label>
+            <Field name="id_Experience" as="select" id="id_Experience">
+              <option value={0}>0-2 months</option>
+              <option value={1}>2-6 months</option>
+              <option value={2}>6-12 monts</option>
+              <option value={3}>1-2 years</option>
+              <option value={4}>2-4 years</option>
+            </Field>
+          </div>
+          <div>
+            <label className="formTitle" htmlFor="id_Speciality">
+              Speciality
+            </label>
+            <Field name="id_Speciality" as="select" id="id_Speciality">
+              <option value={0}>Administrador de Base de Datos/DBA</option>
+              <option value={1}>Administrador de Infraestructura</option>
+              <option value={2}>Administrador de Redes</option>
+            </Field>
+          </div>
 
-            {!sendingForm && (
-              <div className="btnContainer">
-                <button type="submit" className="publishButton">
-                  Publicar
-                </button>
-              </div>
-            )}
-            {sendingForm && (
-              <p className="ofertaPublicada">Oferta publicada!</p>
-            )}
-          </Form>
-        )}
-      </Formik>
-    </Container>
+          {!sendingForm && (
+            <div className="btnContainer">
+              <button type="submit" className="publishButton">
+                Publicar
+              </button>
+            </div>
+          )}
+          {sendingForm && <p className="ofertaPublicada">Oferta publicada!</p>}
+        </Form>
+      )}
+    </Formik>
   );
 };
 
